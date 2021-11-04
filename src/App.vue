7df224088d03ee06dc1c9527da5b7764507e9be0
Shimon Dotera <dotera.s@tam-tam.co.jp>
2021-11-04 19:30:39 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { reactive } from '@vue/reactivity'
+import Choices from './components/Choices.vue'
+
+const choiceList = reactive([
+  {
+    date: '2021-10-04',
+    name: 'カップ麺'
+  },
+  {
+    date: '2021-10-05',
+    name: 'マクドナルド'
+  }
+])
+function addItem(name: string) {
+  choiceList.push({
+    date: '2021-10-02',
+    name: name
+  })
+}
+</script>
+
+<template>
+  <h1>ふーちょ！</h1>
+  <p>今日のご飯なにする？アプリ</p>
+  <!-- 選択肢部分 -->
+  <Choices :list="choiceList" v-on:add="addItem" />
+  <!-- ルーレット部分 -->
+  <!-- ログ部分 -->
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Choices.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  props: {
+    list: { default: [] }
+  },
+  setup() {
+    const input = ref('')
+
+    // function addChoice() {
+    //   // alert(input.value)
+      
+    // }
+
+    return {
+      input,
+      // addChoice
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- 一応選択肢出す -->
+  <ul>
+    <li v-for="item,index in list" :key="index">
+      {{ item.name }}
+    </li>
+  </ul>
+  <input type="text" v-model="input" placeholder="新規追加">
+  <button @click="$emit('add', input.value)">追加</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
